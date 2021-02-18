0fded203d9372bdf253ed44c2676e768722e8374
ruslandavydov <Guinness2374637>
2021-02-18 15:01:33 +0300

    2. Каркас шахматной доски [#444341]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -7,24 +7,36 @@ import ru.job4j.chess.firuges.Figure;
 public class BishopBlack implements Figure {
     private final Cell position;
 
-    public BishopBlack(final Cell ps) {
-        position = ps;
+    public BishopBlack(final Cell position) {
+        this.position = position;
     }
 
     @Override
     public Cell position() {
-        return position;
+        return this.position;
     }
 
     @Override
     public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not way by diagonal from %s to %s", position, dest)
+            );
+        }
+        int deltaX = position.getX() > dest.getX() ? -1 : 1;
+        int deltaY = position.getY() > dest.getY() ? -1 : 1;
+        int size = Math.abs(position.getX() - dest.getX());
+        Cell[] steps = new Cell[size];
+        for (int i = 0; i < size; i++) {
+            int x = position.getX() + (i + 1) * deltaX;
+            int y = position.getY() + (i + 1) * deltaY;
+            steps[i] = Cell.values()[8 * x + y];
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
+        return (Math.abs(source.getX() - dest.getX()) == Math.abs(source.getY() - dest.getY()));
     }
 
     @Override
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -3,16 +3,107 @@ package ru.job4j.chess;
 import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
-import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.*;
+import ru.job4j.chess.firuges.white.PawnWhite;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class LogicTest {
 
-    @Ignore
     @Test
-    public void move()
+    public void whenBishopBlackMoveRightUpThenTrue()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
+        logic.add(new BishopBlack(Cell.F8));
+        logic.move(Cell.F8, Cell.D6);
+    }
+
+    @Test
+    public void whenBishopBlackMoveLeftDownThenTrue()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.D6));
+        logic.move(Cell.D6, Cell.F8);
+    }
+
+    @Test
+    public void whenBishopBlackPositionFinishEqStart() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell result = bishopBlack.position();
+        Cell expect = Cell.C1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenBishopBlackWayThenCellSteps() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] result = bishopBlack.way(Cell.G5);
+        Cell[] expect = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenKingBlackMoveForwardThenTrue()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new KingBlack(Cell.E8));
+        logic.move(Cell.E8, Cell.E7);
+    }
+
+    @Test
+    public void whenRookMoveForwardThenTrue()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new RookBlack(Cell.G6));
+        logic.move(Cell.G6, Cell.G3);
+    }
+
+    @Test
+    public void whenRookMoveRightThenTrue()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new RookBlack(Cell.G6));
+        logic.move(Cell.G6, Cell.D6);
+    }
+
+    @Test
+    public void whenPawnBlackMoveForwardThenTrue()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new PawnBlack(Cell.G7));
+        logic.move(Cell.G7, Cell.G6);
+    }
+
+    @Test
+    public void whenPawnWhiteMoveForwardThenTrue()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new PawnWhite(Cell.A2));
+        logic.move(Cell.A2, Cell.A3);
+    }
+
+    @Test
+    public void whenPawnBlackWayThenCellSteps() {
+        PawnBlack pawnBlack = new PawnBlack(Cell.A7);
+        Cell[] result = pawnBlack.way(Cell.A6);
+        Cell[] expect = {Cell.A6};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenQueenBlackMoveForwardThenTrue()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new QueenBlack(Cell.E8));
+        logic.move(Cell.E8, Cell.E3);
+    }
+
+    @Test
+    public void whenKnightBlackMoveForwardThenTrue()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new KnightBlack(Cell.F5));
+        logic.move(Cell.F5, Cell.G7);
     }
 }
